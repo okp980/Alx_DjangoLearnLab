e819d6bc61c6dae41bff85e45a6ea70e3df06a9b
Okpunor Emmanuel <okpunorrex@gmail.com>
2025-09-07 18:49:00 +0300

    Update settings.py to include 'relationship_app' in template directories, enabling template loading for the app.

--- django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/LibraryProject/LibraryProject/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'LibraryProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['relationship_app'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

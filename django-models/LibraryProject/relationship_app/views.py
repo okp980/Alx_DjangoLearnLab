d936ebdc5c2dc1df002e94bda74b5ce1284daf00
Okpunor Emmanuel <okpunorrex@gmail.com>
2025-09-07 18:34:59 +0300

    Update import statement in views.py to use specific import path for DetailView, enhancing clarity and consistency.

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Library
 from .models import Book
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 
 # Create your views here.
 

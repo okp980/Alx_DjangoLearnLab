fd696e3978bf6ed73d17a3aadf3db10a6e10845e
Okpunor Emmanuel <okpunorrex@gmail.com>
2025-09-07 18:36:11 +0300

    Refactor import statements in urls.py to enhance clarity by separating the imports for list_books and LibraryDetailView.

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import LibraryDetailView, list_books
+from .views import list_books
+from .views import LibraryDetailView
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),

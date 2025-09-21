291f7dffc0714b77b5ff20fcee73b68ae7269ad7
Okpunor Emmanuel <okpunorrex@gmail.com>
2025-09-21 11:18:34 +0300

    Update settings.py to include additional template directory; add user registration view in views.py for improved user authentication.

--- django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/LibraryProject/LibraryProject/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'LibraryProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['relationship_app'],
+        'DIRS': ['relationship_app', BASE_DIR / "templates" ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from .models import Library
 from .models import Book
 from django.views.generic.detail import DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 # Create your views here.
 
@@ -14,4 +19,8 @@ class LibraryDetailView(DetailView):
     template_name = 'relationship_app/library_detail.html'
     
 
-    
+
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/register.html'

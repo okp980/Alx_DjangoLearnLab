8e9fa2475ae49b6d2c94503f9ad15203014dccc5
Okpunor Emmanuel <okpunorrex@gmail.com>
2025-09-21 11:22:30 +0300

    Refactor registration view in views.py and update urls.py to use the new function-based register view; adjust imports accordingly.

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,6 +7,8 @@ from django.contrib.auth.forms import UserCreationForm
 
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 
@@ -20,7 +22,12 @@ class LibraryDetailView(DetailView):
     
 
 
-class RegisterView(CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'registration/register.html'
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('login'))
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})

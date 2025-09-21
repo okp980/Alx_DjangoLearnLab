22df11a4e9ebd73d7a0f37278540af08dfde66e0
Okpunor Emmanuel <okpunorrex@gmail.com>
2025-09-21 11:13:11 +0300

    Add login, logout, and register templates; update urls.py to include authentication paths

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from .views import list_books
 from .views import LibraryDetailView
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+
 ]

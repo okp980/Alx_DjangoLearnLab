6a5e8e722433b06718157870ab18283e819102c9
Okpunor Emmanuel <okpunorrex@gmail.com>
2025-09-27 21:25:13 +0300

    Add initial API app structure with models, serializers, and views
    - Created the 'api' application with models for Author and Book.
    - Implemented serializers for both models to facilitate data conversion.
    - Developed viewsets for CRUD operations on Author and Book resources.
    - Configured URL routing to include API endpoints for the new app.
    - Updated project settings to integrate Django REST framework and filtering capabilities.
    - Established initial database migrations for the new models.

--- advanced-api-project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    
+    'rest_framework',
+    'django_filters',
+    'api'
 ]
 
 MIDDLEWARE = [
@@ -120,3 +124,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
